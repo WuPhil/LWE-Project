85c5e9d4831d3be60316022a46ddd9486bab2c21
Philip Wu <philipwu300@gmail.com>
2020-07-08 12:54:02 -0400

    Protocol is now correct, readme has been updated

--- LWE_Encryption.py
+++ LWE_Encryption.py
@@ -1,135 +1,65 @@
-#An attempt at LWE
-
-#Encryption
-
-#The actual scheme works over a prime on modular arithmetic
-#Public parameters: A 1xn (arbitrary) dimensional matrix with random entries (A), a prime modulus (q) 
-#Private variables: the bit to send over (M), a secret integer (s), and a matrix of random small errors (e)
-#A new matrix B is made with the equation B = s*A + e for each entry in A
-#Take the sum of random samples of the values in A (u) and B (v), but add M/2 to B if the bit to encrypt in B is 1
-#More public values: the values u and v described above
-
-#Decryption
-#Calculate v - su for all of the bits
-#If the value is above 1/2, return 1
-#Else return 0
-
-#Sidenotes
-#We can encrypt an entire message in this program, but it iterates bit by bit
-#So one character in the message results in 8 bits, making the results can seem a "bit" large
+#LWE Encryption Attempt
 
 import numpy as np
 import sys
 import random
 import math
 
-#Important methods
-
-#This is the random sampler that we repeat for each bit
-#It also calculates u and v
-def get_uv(A,B,bit,q):
-	u=0
-	v=0
-	sample= random.sample(range(nvals-1), nvals//4)
-	#If you want to see the random sampling at hand, uncomment the next line:
-	#print(sample)
-	for x in range(0,len(sample)):
-		u=u+(A[sample[x]])
-		v= v+B[sample[x]]
-	v=v+math.floor(q/2)*bit
-	return u%q,v%q
-
-#This is the decryption method
-def get_result(u,v,q):
-    #If you would like to see more of the decryption, uncomment lines in this method
-	res=(v-s*u) % q
-	#print(res)
-	#print(q)
-	if (res>q//2):
-		return 1
-	return 0
-
-#Convert each character into a string
-def tobits(val):
-	l = [0]*(8)
-	l[0]=val & 0x1
-	l[1]=(val & 0x2)>>1
-	l[2]=(val & 0x4)>>2
-	l[3]=(val & 0x8)>>3
-	l[4]=(val & 0x10)>>4
-	l[5]=(val & 0x20)>>5
-	l[6]=(val & 0x40)>>6
-	l[7]=(val & 0x80)>>7
-	return l
-
 ##Prompting user input
 print("All the values entered should be positive integers\n")
-q = int(input("How big should the \"ring\" (modulus number) be? This should be prime) "))
-s = int(input("What's the secret number? (This should be between 0 and q-1) "))
-nvals = int(input("How many elements should the public matrix have? (should be >=16; compiler clutters w/ big values) "))
-s = s % q
-string = input("What message should be encrypted? ")
+q = int(input("Modulus size (q): "))
+n = int(input("Number of variables (n): "))
+b = int(input("Error bound (b, typically q/4): "))
 
-A = []
-B = []
-e = []
-M = []
+#Key generator method
+#creates a key for the other methods
 
-#Creating parameters A,B,e
-A = random.sample(range(q), nvals)
+def keyGen(n,q):
+    x = []
+    for i in range(n):
+        x.append(random.randint(0,q-1))
+    return x
 
-for x in range(0,len(A)):
-	e.append(random.randint(0,3))
-	B.append((A[x]*s+e[x])%q)
+def enc(n,q,x,m):
+    a = []
+    y = 0
+    for i in range(n):
+        a.append(random.randint(0,q-1))
+    for i in range(n):
+        y += a[i] * x[i] + random.randint(0, b//(n**2)+1)
+    y += m*q//2 % q
+    y %= q
+    return a,y
 
-#Start converting all of the message to bits
-for c in string:
-    M.append(tobits(ord(c)))
-    
-#Outlook on what the method does
-print("\n------Parameters and keys-------")
-print("Message to cipher:\t",string)
-print("Public Key (A):\t",A," and its size is ",len(A))
-print("Public Key (B):\t",B)
-print("Errors (e):\t\t",e)
-print("Secret key:\t\t",s)
-print("Prime number:\t\t",q)
+def dec(n,q,b,x,a,y):
+    w = 0
+    for i in range(n):
+        w += a[i] * x[i]
+    z = (y-w) % q
+    print(z)
+    if z<=b:
+        return 0
+    else:
+        return 1
 
-print("\n------Sampling Process from public key-------")
-#Iterate through all bits, get u and v for each
-uentries = []
-ventries = []
+##Subroutine 1: check if a 0 and 1 bit work with correctness
 
-for byte in M:
-    for bit in byte:
-        u,v = get_uv(A,B,bit,q)
-        uentries.append(u)
-        ventries.append(v)
+x = keyGen(n,q)
 
-#To hide the big final matrices, comment these lines
-print("Entries in u: ", uentries)
-print("Entries in v: ", ventries)
+#Testing a 0 bit
+a0,y0 = enc(n,q,x,0)
+print("0 bit\na = ", a0,"\ny = ", y0, "\n")
+#Testing a 1 bit
+a1,y1 = enc(n,q,x,1)
+print("1 bit\na = ", a1,"\ny = ", y1, "\n")
 
-print("\n------Results-----------------")
-#This is the decryption method
-decryptedmatrix = []
-for i in range(len(uentries)):
-    decryptedmatrix.append(get_result(uentries[i],ventries[i],q))
-    
-print("Bits at the end: ", decryptedmatrix)
 
-#Finally turning these bits into characters
-bytelist = [decryptedmatrix[i:i+8] for i in range(0, len(decryptedmatrix), 8)]
-charlist = []
+if(dec(n,q,b,x,a0,y0) == 0):
+    print("0 successfully decoded")
+else:
+    print("0 failed")
 
-for arr in bytelist:
-    a = 0
-    for i in range(len(arr)):
-        a += (2**i)*arr[i]
-    charlist.append(chr(a))
-
-x = ""
-for c in charlist:
-    x += c
-    
-print("Resulting message is: ", x)
+if(dec(n,q,b,x,a1,y1) == 1):
+    print("1 successfully decoded")
+else:
+    print("1 failed")
--- README.md
+++ README.md
@@ -1,17 +1,34 @@
 # LWE-Project
 
-A very primitive LWE system using Python; probably has a load of issues.
+A very primitive LWE system using Python; possible with many problems.
 
-## Known Issues
+##Plans/Ideas
 
-Currently none, but some conditions should be met within the inputs of the encryption scheme.
+* Add more user interaction - allow them to perform chosen-plaintext attacks
+* Add subroutines to check the validity of the cryptosystem
+* Create methods for encrypting words/images
+* Implement more attacks
 
-## Checklist/Plans
+##Methodology
 
-* Create the public & private systems
-* Implement some attacks
+Parameters: a prime modulus *q*, the number of dimensions *n*, and acceptable error bound *b*.  
 
-## Authors
+LWE is based on multiplying random coefficients over a public key and then adding small errors over modular arithmetic.
+This is proven to be at least as hard as worst-case lattice problems on its average case, and is thought to be exponentially hard even for quantum computers to solve.
+
+##Usage
+
+The program is intended to be run in a shell, and will prompt for the parameters.
+1) Use the keyGen method to create a key.  
+2) Use the enc method on the key and a message bit to encrypt data.  
+3) Use the dec method with the key and encrypted data to get the message bit back.
+
+###Methods:
+* keyGen(n,q) - generates a secret vector *x* (the "key") which will be used in the other methods.
+* enc(n,q,x,m) -  Encrypts the message bit *m*. *x* will be multiplied with random coefficients and added with error, and *m* * *q*/2 will be added .
+* dec(n,q,b,x,a,y) - decrypts the ciphertext and outputs the message bit *m*. It calculates *z*=*y*-*a* * *x*, and outputs the bit based on the accepted error bound.
+
+##Contributors
 
 **Gorjan Alagic** - *Mentor*  
 **Philip Wu** - *Student*  
