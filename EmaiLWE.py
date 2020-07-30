e4a0aea260462fbfe723fabd9abd8e615a69f8db
Philip Wu <philipwu300@gmail.com>
2020-07-30 08:11:07 -0400

    Created a rough version of EmaiLWE

--- EmaiLWE.py
+++ EmaiLWE.py
@@ -7,7 +7,7 @@ import random
 import math
 
 print("EmaiLWE: A tool to create a public message system with LWE to use over email\n")
-print("This program has the option to\n(1) generate keys\n(2) encrypt with a plaintext file\n(3) decrypt from a ciphertext file")
+print("This program can\n(1) generate keys\n(2) encrypt a bitstring with a public key file\n(3) decrypt from a ciphertext file")
 choice = int(input("Choose which option you would like to take: "))
 
 if choice == 1:
@@ -21,14 +21,14 @@ if choice == 1:
     for i in range(n):
         x.append(random.randint(0,q-1))
 
+    #Writing to files
     f = open("secret.txt", "w")
-        f.write("Mod =" + q + "\nVector size =" + n + "\nEquations =" + q + "\n")
-
+    f.write("q=" + str(q) + "\nn=" + str(n) + "\nm=" + str(m) + "\n")
     f.write(str(x))
     f.close()
 
     f = open('pk.txt', 'w')
-    f.write("Mod =" + q + "\nVector size =" + n + "\nEquations =" + q + "\n")
+    f.write("q=" + str(q) + "\nn=" + str(n) + "\nm=" + str(m))
     
     #Public Key creation 
     for i in range(m):
@@ -38,13 +38,105 @@ if choice == 1:
         y = numpy.dot(a,x)
         y == random.randint(0, b)
         y %= q 
-        f.write(str([a,y]) + "\n")
+        f.write("\n" + str(a))
+        f.write("\n" + str(y))
 
+    f.close()
 
-#elif choice == 2:
-    #Add file reading for q,n,m
-    #randomly sample through all of the equations for every bit to encrypt
-    #write out the equations
+elif choice == 2:
+    
+    #Reading parameters
+    f = open('pk.txt', 'r')
+    s = f.readline()
+    q = int(s[2:len(s)-1])
+    s = f.readline()
+    n = int(s[2:len(s)-1])
+    s = f.readline()
+    m = int(s[2:len(s)-1])
+    
+    lines = f.read()
+    equations = lines.split("\n")
+    coefficients = []
+    outputs = []
 
-#elif choice == 3:
+    for i in range(m):
+        tempStr = equations[2*i][1:len(equations[2*i])-1]
+        tempCoefficient = tempStr.split(", ")
+        for j in range(n):
+            tempCoefficient[j] = int(tempCoefficient[j])
+        coefficients.append(tempCoefficient)
+        outputs.append(int(equations[2*i+1]))
+
+    bits = input("What bitstring would you like to encrypt?")
+
+    #there definitely needs to be a lower bound on the number of samples, but not really specified yet
+    f = open("cipher.txt", "w")
+    for c in bits:
+        numSamples = random.randint(m//3,m)
+        chosenEquations = []
+        for i in range(numSamples):
+            chosenEquations.append(random.randint(0,m-1))
+        samples = numpy.array([0] * n)
+        sampleOutput = 0
+        for x in chosenEquations:
+            #uncomment any print statement to debug
+            #print(x)
+            #print(samples)
+            #print(coefficients[x])
+            samples += numpy.array(coefficients[x])
+            sampleOutput += outputs[x]
+            
+
+        samples = [s % q for s in samples]
+        sampleOutput += (q//2)*int(c) + random.randint(0,q//4)
+        sampleOutput %= q
+        #print(chosenEquations)
+        #print(samples)
+        #print(sampleOutput)
+        f.write(str(samples) + "\n" + str(sampleOutput) + "\n")
+        #print(str(samples) + "\n" + str(sampleOutput))
+    f.close()
+
+elif choice == 3:
+    #Reading parameters & secret key
+    f = open('secret.txt', 'r')
+    s = f.readline()
+    q = int(s[2:len(s)-1])
+    s = f.readline()
+    n = int(s[2:len(s)-1])
+    s = f.readline()
+    m = int(s[2:len(s)-1])
+    s = f.readline()
+    temp = s[1:len(s)-1]
+    x = temp.split(", ")
+    x = [int(a) for a in x]
+    print(q,n,m,x)
+
+    #Testing the ciphertext equations for decryption
+    f = open('cipher.txt', 'r')
+    lines = f.read()
+    equations = lines.split("\n")
+    coefficients = []
+    outputs = []
+
+    for i in range(len(equations)//2):
+        tempStr = equations[2*i][1:len(equations[2*i])-1]
+        tempCoefficient = tempStr.split(", ")
+        for j in range(n):
+            tempCoefficient[j] = int(tempCoefficient[j])
+        coefficients.append(tempCoefficient)
+        outputs.append(int(equations[2*i+1]))
+    #print(coefficients)
+    #print(outputs)
+
+    dec = ""
+    for i in range(len(coefficients)):
+        z = (outputs[i] - numpy.dot(coefficients[i],x)) % q
+        if z<=q//2:
+            dec += "0"    
+        else:
+            dec += "1"
+
+    print("The decrypted message is: ", dec)
+    
 
--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # LWE-Project
 
-A very primitive LWE system using Python.
+Implementing a very primitive LWE system using Python, and additionally using a public key system over email to encrypt messages.
 
 ## Plans/Ideas
 
-* Add more user interaction - allow them to perform chosen-plaintext attacks
-* Add subroutines to check the validity of the cryptosystem
-* Create methods for encrypting words/images
-* Implement more attacks
+-Basic Cryptosystem
+* Add more types of encryptions
+* Implement some more attacks
+
+-EmaiLWE
+* Do some code cleaning (add methods, use the random sample method...)
+* Add more types of encryptions
 
 ## Methodology
 
@@ -16,6 +19,7 @@ Parameters: a prime modulus *q*, the number of dimensions *n*, and acceptable er
 LWE is based on multiplying random coefficients over a public key and then adding small errors over modular arithmetic.
 This is proven to be at least as hard as worst-case lattice problems on its average case, and is thought to be exponentially hard even for quantum computers to solve.
 
+# LWE_Encryption
 ## Usage
 
 The program is intended to be run in a shell, and will prompt for the parameters.
@@ -25,7 +29,7 @@ The program is intended to be run in a shell, and will prompt for the parameters
 
 ### Construction Methods:
 * keyGen(n,q) - Generates a secret vector *x* (the "key") which will be used in the other methods.
-* enc(n,q,x,m) -  Encrypts the message bit *m*. *x* will be multiplied with random coefficients and added with error, and *m* * *q*/2 will be added .
+* enc(n,q,x,m) -  Encrypts the message bit *m*. *x* will be multiplied with random coefficients and added with error, and *m* * *q*/2 will be added.
 * dec(n,q,b,x,a,y) - Decrypts the ciphertext and outputs the message bit *m*. It calculates *z*=*y*-*a* * *x*, and outputs the bit based on the accepted error bound.
 
 ### Subroutines:
@@ -36,7 +40,18 @@ The program is intended to be run in a shell, and will prompt for the parameters
 ### Attacks:
 * generateBruteForce(n,q,b) - Generates a key that the code attacks by requesting many samples and algorithmically checking every value. By default, a key is considered correct if it properly decrpyts 20 *0* bits and 20 *1* bits.
 * sampleBruteForce(n,q,m,b) - Takes *m* * *n* coefficients as the first user input (the first *n* coefficients correspond to the first equation) and *m* outputs to each equation, and finally a third prompt for if there is an expected message. If there is an expected message, then the method solves for the key, otherwise, it will show the most likely key.
-* chosenCiphertestAttack(n,q,b) - Generates a secret key and uses the decryption oracle to crack it quickly. Does this by entering ciphertexts with only one coefficient and using the error bounds to find the value of that coefficient in the secret key..
+* chosenCiphertestAttack(n,q,b) - Generates a secret key and uses the decryption oracle to crack it quickly. Does this by entering ciphertexts with only one coefficient and using the error bounds to find the value of that coefficient in the secret key.
+
+# EmaiLWE
+This program was developed to act as an encryption protocol over email using LWE. One person creates a secret and public key (which are secret.txt and pk.txt respectively) and emails only pk.txt to their acquaintance. The other person can use the encrypion method on a bitstring after downloading pk.txt and send over the ciphertext (cipher.txt), which the other person decrypts after downloading.
+
+## Usage
+Bob wants to share a message to Alice.
+1) Alice uses types "1" with the program to make secret.txt and pk.txt.  
+2) Alice shares pk.txt with Bob.  
+3) Bob types "2" with the program and enters a bitstring he wants to encrypt, which makes cipher.txt.  
+4) Alice types "3" with the program and gets Bob's message
+
 ## Contributors
 
 **Gorjan Alagic** - *Mentor*  
